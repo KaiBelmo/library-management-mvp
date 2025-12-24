6625ffd051b0ded030ce9eef246da55d5ed79e9b
KaiBelmo <belmomohamedali@gmail.com>
2025-12-24 18:12:39 +0700

    feat: add registerUser to test-utils

--- backend/testing/config/test-utils.js
+++ backend/testing/config/test-utils.js
@@ -12,7 +12,19 @@ const loginAsAdmin = async () => {
   return await login(TEST_CONFIG.ADMIN_EMAIL, TEST_CONFIG.ADMIN_PASSWORD);
 };
 
+const registerUser = async (userData) => {
+  return await request(TEST_CONFIG.DIRECTUS_URL)
+    .post('/users/register')
+    .send({
+      email: userData.email,
+      password: userData.password,
+      first_name: userData.first_name,
+      last_name: userData.last_name
+    });
+};
+
 module.exports = {
   login,
   loginAsAdmin,
+  registerUser,
 };
