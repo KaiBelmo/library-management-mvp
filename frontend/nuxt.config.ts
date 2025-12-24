4bcae0dd54f9934487d59371fe71b95b6bab9211
KaiBelmo <belmomohamedali@gmail.com>
2025-12-25 01:02:01 +0700

    chore: reseting nuxt modules and installing tailwind css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxtjs/tailwindcss',
-    'nuxt-directus'
-  ]
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
