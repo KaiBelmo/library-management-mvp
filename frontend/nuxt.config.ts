7a7630abc9ebeb57d8030ae27298cfa62db2ee8a
KaiBelmo <belmomohamedali@gmail.com>
2025-12-28 05:11:49 +0700

    chore: removing tailwind since nuxt ui comes with it

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,16 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
 
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
   directus: {
     url: process.env.DIRECTUS_URL,
     autoFetch: true,
@@ -24,5 +17,11 @@ export default defineNuxtConfig({
     }
   },
 
-  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-directus', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    'nuxt-directus',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/ui'
+  ],
 })
